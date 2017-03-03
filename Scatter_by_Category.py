bd95816a09a96a025ee589510a12db6b516c9415
jliu88 <improbabilitydrive@gmail.com>
2017-03-02 16:33:51 -0800

    Create Scatter_by_Category.py

---
+++ Scatter_by_Category.py
@@ -0,0 +1,10 @@
+#!usr/bin/env python
+
+import os.path
+import xml.etree.ElementTree as ET
+import pandas as pd
+from bokeh.io import curdoc
+from bokeh.layouts import widgetbox, row, column
+from bokeh.models.widgets import Select
+from bokeh.plotting import figure, ColumnDataSource
+from bokeh.models import HoverTool
