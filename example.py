75771a2b5a4b901271f9fced6f337dcefb0e8b52
jliu88 <improbabilitydrive@gmail.com>
2017-03-16 10:09:59 -0700

    Add files via upload

---
+++ example.py
@@ -0,0 +1,31 @@
+#!usr/bin/env python
+import analysis_console as ac
+from bokeh.sampledata.autompg import autompg
+from bokeh.sampledata.us_marriages_divorces import data
+
+#Choose example
+example = 3
+
+if example == 1: #Basic scatter chart
+    x = ac.tools(df=autompg,
+                metric=list(autompg.columns),
+                xaxis='weight'
+                )
+    x.run()
+
+
+
+if example == 2: #Scatter chart with different colors for categories
+    x = ac.tools(df=autompg,
+                metric=list(autompg.columns),
+                xaxis = 'weight', 
+                cato = list(autompg.columns)
+                )
+    x.run()
+    
+if example ==3: #Example of divorces over time
+    x = ac.tools(df=data,
+                metric=list(data.columns),
+                xaxis='Year'
+                )
+    x.run()
